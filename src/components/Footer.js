7369f2a10cef0bf36d31d018447cce93a60dd6e2
Vera Zhurba <vera.zhurba12@gmail.com>
2024-04-03 15:28:32 +0200

    footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -31,36 +31,6 @@ const Footer = () => {
             </li>
           );
         })}
-        <li>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            className="footer-icon"
-            rel="noreferrer"
-          >
-            <i className="fab fa-facebook"></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            className="footer-icon"
-            rel="noreferrer"
-          >
-            <i className="fab fa-twitter"></i>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            className="footer-icon"
-            rel="noreferrer"
-          >
-            <i className="fab fa-squarespace"></i>
-          </a>
-        </li>
       </ul>
       <p className="copyright">
         copyright &copy; Backroads travel tours company
